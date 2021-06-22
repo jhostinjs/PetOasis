2b4305d0ca2c8c2bc2bec08029e30f659e1cbf15
jhostinjs <jhostin.juradosoto@gmail.com>
2021-06-22 16:27:01 -0500

    Proyecto acabado

--- PetOasis/Models/Alimento.cs
+++ PetOasis/Models/Alimento.cs
@@ -11,7 +11,7 @@ namespace PetOasis.Models
         [Display (Name = "Codigo")]public int codigo { get; set; }
         [Display(Name = "Nombre")] public string nombre { get; set; }
         [Display(Name = "Precio")] public decimal precio { get; set; }
-        [Display(Name = "Cantidad")] public int cantidad { get; set; }
+        [Display(Name = "Stock")] public int cantidad { get; set; }
         [Display(Name = "Tipo")] public int tipo { get; set; }
         [Display(Name = "Disponibilidad")] public int disponibilidad { get; set; }
         [Display(Name = "Imagen")] public string imgAli { get; set; }
